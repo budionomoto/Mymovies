c7784784a73cfec679d6e7dc158f15decb282c13
budionomoto <budionomotoe3@gmail.com>
2018-01-22 22:44:53 +0700

    22.44

--- app/src/main/java/id/xyzsystem/budiono/mymovies/MainActivity.java
+++ app/src/main/java/id/xyzsystem/budiono/mymovies/MainActivity.java
@@ -2,43 +2,15 @@ package id.xyzsystem.budiono.mymovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import org.greenrobot.eventbus.EventBus;
 
 
-// ini file pertama yg dibuat
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.daftar)
-    RecyclerView moviesDaftar;
-
-    private EventBus eventBus = App.getInstance().getEventBus();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        eventBus.register(this);
 
-        initView();
-    }
-
-    private void initView(){
-        ButterKnife.bind(this);
-
-        moviesDaftar.setLayoutManager(new LinearLayoutManager(this));
-        moviesAdapter = new MoviesAdapter();
-        moviesDaftar.setAdapter(moviesAdapter);
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        eventBus.unregister(this);
-        super.onDestroy();
     }
 
 }
